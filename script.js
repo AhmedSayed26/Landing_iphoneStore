0d2b7f3427cf9d347696f731bf60d471455baf7c
ahmed sayed <22ahmed23sayed24@gmail.com>
2024-11-05 00:31:13 +0200

    iphone store

---
+++ script.js
@@ -0,0 +1,8 @@
+let img = document.querySelector('.img')
+let colorss = document.querySelector('.countainuer') 
+function iphones(iphone){
+    img.src = iphone
+}
+function colors(color){
+    colorss.style.background = color
+}
